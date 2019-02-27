72c157d6f78d1ecee419a4e8f51c061e7a6e102e
recursivefunk <johnny@johnnyray.me>
2019-02-27 13:23:02 -0500

    throw if the appopriate env vars are not set

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ const region = env.get('AWS_DEFAULT_REGION', 'us-east-1')
 const cloudwatch = new AWS.CloudWatch({ region })
 
 const FlyMetrix = function (namespace = 'FlyMetrix') {
+
+  // Ensure the required environment variables are set. Throw, if not.
+  env.ensure(
+    'AWS_ACCESS_KEY_ID',
+    'AWS_SECRET_ACCESS_KEY'
+  )
+
   return Object.create({
     Counter: (name = 'counter') => createCounter(name, namespace, cloudwatch),
     Timer: (name = 'timer') => createTimer(name, namespace, cloudwatch)
